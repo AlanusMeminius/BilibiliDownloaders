e6fe1b71e3bf08c96ea331a85dc1b5d414f68002
ives <1660583890@qq.com>
2022-09-09 21:22:08 +0800

    add result

---
+++ x64/Release/killprocess.cmd
@@ -0,0 +1,17 @@
+echo kill process start
+
+echo kill BilibiliDownloaders
+taskkill /IM BilibiliDownloaders.exe /F
+
+echo kill ffmpeg
+taskkill /IM ffmpeg.exe /F
+
+echo kill ffmpeg
+taskkill /IM ffmpeg.exe /F
+
+echo kill aria2c
+taskkill /IM aria2c.exe /F
+
+echo kill process end
+
+pause
